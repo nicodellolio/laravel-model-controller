c46385dbc9151f7e97704de67123da3aea4cc303
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-09 16:51:16 +0200

    add header style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>LaraMovie</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>LaraMovie</title>
 
-    </head>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
 
 
-    <body class="antialiased">
+<body class="antialiased">
 
-        @include('partials.header')
+    @include('partials.header')
 
-        @yield('content')
+    @yield('content')
 
-        @include('partials.footer')
+    @include('partials.footer')
 
-    </body>
+</body>
+
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,32 @@
-
 <header>
-    <h5>Here goes the header</h5>
+    <nav class="navbar navbar-expand-sm navbar-light bg-warning">
+
+        <div class="container-fluid d-flex justify-content-between px-5">
+
+            <a class="navbar-brand ms-4" href="#">
+                <div class="logo">
+                    <img style="width: 150px" class="p-2"
+                        src="https://static.vecteezy.com/system/resources/previews/008/505/808/original/cinema-projector-and-film-strip-illustration-png.png"
+                        alt="">
+                </div>
+            </a>
+            <ul class="navbar-nav d-flex fs-4 ">
+                <li class="nav-item fw-semibold">
+                    <a class="nav-link active" aria-current="page" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+
+            <form class="d-flex me-4" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+        </div>
+    </nav>
 </header>
