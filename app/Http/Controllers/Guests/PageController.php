42d6bc07e07fd73264bfeff723878d26e5d5c05b
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-09 15:58:38 +0200

    first commit

---
+++ app/Http/Controllers/Guests/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Guests;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+}
